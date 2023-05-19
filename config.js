4c0c5b405ca490d583bdb1b4d0edf213c397d5d0
Lucas2k4 <128827227+Lucas2k4@users.noreply.github.com>
2023-05-19 14:23:43 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'younger sister';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100061151645547&mibextid=LQQJ4d';
+const eventName = 'Happy Birthday';
+const titleCard = 'Happy Birthday';
+const contentCard = 'Today is your birthday. I hope you can make dream come true and happy with family.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'img-b.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
